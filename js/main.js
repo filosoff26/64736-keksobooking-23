c53994700764203a1de55002e5c52bddaf281a14
Alexey Stepanenko <filosoff26@gmail.com>
2021-07-14 18:57:17 +0700

    Добавлена функция создания 10 случайных предложений об аренде, m4-t1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*
-* Функция получения случайного целого в диапазоне [min, max] или наоборот, если min > max
-* Источник: Mozilla Developer Network (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
-* */
+ * Функция получения случайного целого в диапазоне [min, max] или наоборот, если min > max
+ * Источник: Mozilla Developer Network (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+ */
 function getRandomInt(min, max) {
   if (min > max) {
     const tmp = min;
@@ -32,8 +32,8 @@ function ceil10(number, precision = 0) {
 }
 
 /*
-* Функция получения случайного числа с точностью precision в диапазоне [min, max] или наоборот, если min > max
-* */
+ * Функция получения случайного числа с точностью precision в диапазоне [min, max] или наоборот, если min > max
+ */
 function getRandomFloat(min, max, precision) {
   if (min > max) {
     const tmp = min;
@@ -48,3 +48,80 @@ function getRandomFloat(min, max, precision) {
   return randomNumber;
 }
 getRandomFloat(0.1, 9.9, 1);
+
+
+/*
+ * Вспомогательные функции для получения одного или нескольких случайных элементов массива
+ */
+function getRandomArrayElement (array) {
+  return array[getRandomInt(0, array.length - 1)];
+}
+
+function getRandomArraySubset (array) {
+  const start = getRandomInt(0, array.length - 1);
+  const end = getRandomInt(start + 1, array.length);
+  return array.slice(start, end);
+}
+
+/*
+ * Создание 10 случайных предложений об аренде
+ */
+function generateOffers () {
+  const offerTypes = [
+    'palace',
+    'flat',
+    'house',
+    'bungalow',
+    'hotel',
+  ];
+  const checkinVariants = [
+    '12:00',
+    '13:00',
+    '14:00',
+  ];
+  const featuresVariants = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
+  ];
+  const photosVariants = [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  ];
+  const offers = [];
+
+  for (let offerNumber = 1; offerNumber <= 10; offerNumber++) {
+    const avatarFileNumber = (offerNumber < 10 ? '0' : '') + offerNumber;
+    const location = {
+      lat: getRandomFloat(35.65000, 35.70000, 5),
+      lng: getRandomFloat(139.70000, 139.80000, 5),
+    };
+    const offerType = getRandomArrayElement(offerTypes);
+    const offerRooms = getRandomInt(1, 5);
+    offers.push({
+      author: {
+        avatar: `img/avatars/user${avatarFileNumber}.png`,
+      },
+      offer: {
+        title: `Offer #${offerNumber}`,
+        address: `${location.lat}, ${location.lng}`,
+        price: getRandomInt(1000, 9999),
+        type: offerType,
+        rooms: offerRooms,
+        guests: getRandomInt(1, 9),
+        checkin: getRandomArrayElement(checkinVariants),
+        features: getRandomArraySubset(featuresVariants),
+        description: `Nice ${offerType} with ${offerRooms} room(s)`,
+        photos: getRandomArraySubset(photosVariants),
+      },
+      location: location,
+    });
+  }
+  return offers;
+}
+
+generateOffers();
